e3b100ca3dc037a99a1f6367e7c77b6da1db4333
qhcks <qhcks04@outlook.com>
2021-09-19 22:02:04 +0900

    주문정보 추가등 여러가지를 추가 함

---
+++ docs/process.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+    <title>테스트</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo"></div>
+        </header>
+        <main>
+        </main>
+        <footer></footer>
+    </div>
+</body>
+</html>
--- docs/script.js
+++ docs/script.js
@@ -1,15 +1,18 @@
 window.onload = function(){
-    additem("감자",1123,23);
-    additem("포도",123,3);
+    const totalEl = document.getElementById('total');
+    document.addEventListener('input', function(e) {
+        if(e.target && e.target.id.startsWith('item')) {
+            var itemnum = e.target.id.replace("item","");
+            var priceEl = document.getElementById("price"+itemnum);
+            var countEl = document.getElementById("item"+itemnum);
+            totalEl.value = parseInt(totalEl.value) + parseInt(priceEl.value)*parseInt(countEl.value);
+        }
+    });
+
+
+    additem("감자",5000,23);
+    additem("포도",1000,3);
     additem("복숭아",123,0);
-    additem("감자",1123,23);
-    additem("포도",123,3);
-    additem("복숭아",123,0);
-    additem("감자",1123,23);
-    additem("포도",123,3);
-    additem("복숭아",123,0);
-    additem("감자",1123,23);
-    additem("포도",123,3);
     additem("복숭아",123,0);
     additem("복숭아",123,0);
     additem("복숭아",123,0);
@@ -32,16 +35,17 @@ function additem(pname,pprice,pleft){
 
     num.innerHTML = tbody.rows.length;
     name.innerHTML = pname;
-    price.innerHTML = pprice;
+
+    var priceEl = document.createElement("span");
+    priceEl.setAttribute('id',"price"+num.innerHTML);
+    priceEl.innerHTML = pprice;
+    price.appendChild(priceEl);
 
     var countEl = document.createElement("input")
     countEl.setAttribute('id',"item"+num.innerHTML);
     countEl.setAttribute('type',"number");
     countEl.setAttribute('min',0);
     countEl.setAttribute('value',0);
-
-
-
     if(pleft == 0) {
         row.classList.add('soldOut');
         vleft = "품절";
@@ -49,4 +53,26 @@ function additem(pname,pprice,pleft){
     }
     count.appendChild(countEl);
     left.innerHTML = vleft;
+}
+
+function getPickUp(){
+    var radioList = document.querySelectorAll("input");
+    var result = "er";
+    radioList.forEach(node => {
+        if(node.getAttribute('type')=="radio") {
+            if(node.checked) {
+                result = node.value;
+            }
+        }
+    });
+    return result;
+}
+
+function pickUpCheck() {
+    const addressForm = document.querySelector('.cAddressCont');
+    if(getPickUp()=="deliver"){
+        addressForm.classList.remove('hide');
+    } else {
+        addressForm.classList.add('hide');
+    }
 }
